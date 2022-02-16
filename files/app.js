51dae3310f0676807853e42d7ff5c3f308e93184
nadiakunkevic <nadyakunkevych@gmail.com>
2022-02-16 12:46:18 +0200

    Initial commit

---
+++ files/app.js
@@ -0,0 +1,62 @@
+// 1. Створити основну папку (main), в яку покласти дві інші папки:
+// перша - online, друга - inPerson
+
+const path = require('path')
+
+const fs = require('fs')
+
+fs.mkdir(path.join(__dirname, 'main'), (err)=>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+fs.mkdir(path.join(__dirname, 'main', 'online'), (err)=>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err)=>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+// 2. Потім створити в вашому головному файлі (для прикладу app.js) два масиви з обєктами
+// user ({. name: "Andrii", age: 22, city: "Lviv" }),
+// відповідно перший - onlineUsers, другий - inPersonUsers;
+
+// 3. і створити файли txt в папках (online, inPerson),
+// в яких як дату покласти юзерів з ваших масивів,
+// але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
+
+let onlineUsers = [
+    { name: "Andrii", age: 22, city: "Lviv" },
+    { name: "Oleg", age: 28, city: "Berlin" },
+    { name: "Anna", age: 20, city: "Ternopil" },
+];
+let inPersonUsers = [
+    { name: "Max", age: 34, city: "Paris" },
+    { name: "Julia", age: 30, city: "Lviv" },
+    { name: "Nazar", age: 18, city: "Lutsk" },
+];
+
+
+fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'file.txt' ),'hello USERS', (err)=> {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+fs.writeFile(path.join(__dirname, 'main', 'online', 'file.txt' ),'hello ONLINE', (err)=> {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
